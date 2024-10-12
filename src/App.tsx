eb17ebddc7dfe6a5cb863d31a9f3e29fede98876
melvinchia3636 <melvinchia623600@gmail.com>
2024-10-12 23:27:29 +0800

    24w41

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,156 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import PathEntry from "./PathEntry";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
-function App() {
-  return <div>App</div>;
+export interface RouteDocs {
+  summary: string;
+  description: string;
+  access: "public" | "protected" | "private";
+  params: {
+    name: string;
+    type: string;
+    required: boolean | string;
+    options?: string[];
+    must_exist?: boolean;
+    description: string;
+  }[];
+  query: {
+    name: string;
+    type: string;
+    required: boolean | string;
+    options?: string[];
+    must_exist?: boolean;
+    description: string;
+  }[];
+  body: {
+    name: string;
+    type: string;
+    required: boolean | string;
+    options?: string[];
+    must_exist?: boolean;
+    description: string;
+  }[];
+  response: {
+    status: number;
+    description: string;
+    body: string;
+  };
 }
 
-export default App;
+interface Route {
+  method: string;
+  path: string;
+  description?: string;
+  docs: RouteDocs | null;
+}
+
+const ApiExplorer = () => {
+  const [routes, setRoutes] = useState<Record<string, Route[]>>({});
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_HOST).then(async (res) => {
+      if (res.ok) {
+        const data = await res.json();
+        if (data.state === "success") setRoutes(data.data);
+      }
+    });
+  }, []);
+
+  return (
+    <main className="bg-neutral-950 w-full h-dvh text-neutral-100 font-[Urbanist] flex">
+      {JSON.stringify(routes) !== "{}" ? (
+        <>
+          <aside className="w-1/3 p-8 border-r-2 border-neutral-800/50 overflow-auto">
+            <h1 className="flex items-center gap-2 whitespace-nowrap mb-12 text-2xl tracking-wider font-bold text-neutral-50">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                aria-hidden="true"
+                role="img"
+                className="text-4xl text-lime-500 iconify iconify--tabler"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m11.414 10l-7.383 7.418a2.091 2.091 0 0 0 0 2.967a2.11 2.11 0 0 0 2.976 0L14.414 13m3.707 2.293l2.586-2.586a1 1 0 0 0 0-1.414l-7.586-7.586a1 1 0 0 0-1.414 0L9.121 6.293a1 1 0 0 0 0 1.414l7.586 7.586a1 1 0 0 0 1.414 0"
+                />
+              </svg>
+              <div>
+                LifeForge<span className="text-3xl text-lime-500"> .</span> API
+              </div>
+            </h1>
+            <div className="space-y-8">
+              {Object.entries(routes).map(([route, paths]) => (
+                <div key={route}>
+                  <h2 className="font-bold tracking-widest uppercase text-zinc-500 mb-4">
+                    {route
+                      .split("-")
+                      .map((e) => (e ? e[0].toUpperCase() + e.slice(1) : "/"))
+                      .join(" ")}
+                  </h2>
+                  {paths.map(({ method, path }) => (
+                    <div key={`${route}-${method}-${path}`} className="py-2">
+                      <span
+                        className={`font-semibold tracking-wide ${
+                          {
+                            GET: "text-green-500",
+                            POST: "text-blue-500",
+                            PATCH: "text-yellow-500",
+                            PUT: "text-orange-500",
+                            DELETE: "text-red-500",
+                          }[method]
+                        }`}
+                      >
+                        {method}
+                      </span>
+                      <span className="ml-2 tracking-wide text-zinc-500">
+                        {path}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </aside>
+          <section className="p-12 w-full overflow-auto">
+            {Object.entries(routes).map(([route, paths]) => (
+              <div className="mt-16" key={route}>
+                <h2 className="text-3xl font-semibold mb-4 tracking-wider relative after:absolute after:top-1/2 after:-translate-y-1/2 pl-5 after:left-0 after:w-1 after:h-10 after:rounded-full after:bg-lime-500">
+                  {route
+                    .split("-")
+                    .map((e) => (e ? e[0].toUpperCase() + e.slice(1) : "/"))
+                    .join(" ")}
+                </h2>
+                <div className="space-y-3 mt-8">
+                  {paths.map(({ method, path, docs }) => (
+                    <PathEntry
+                      key={`${route}-${method}-${path}`}
+                      method={method}
+                      path={path}
+                      docs={docs}
+                    />
+                  ))}
+                </div>
+              </div>
+            ))}
+          </section>
+        </>
+      ) : (
+        <div className="w-full h-full flex items-center justify-center">
+          <Icon
+            icon="svg-spinners:180-ring"
+            className="size-10 text-zinc-500"
+          />
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default ApiExplorer;
---
+++ src/PathEntry.tsx
@@ -0,0 +1,196 @@
+import React, { useState } from "react";
+import { RouteDocs } from "./App";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+function PathEntry({
+  method,
+  path,
+  docs,
+}: {
+  method: string;
+  path: string;
+  docs: RouteDocs | null;
+}) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div className="w-full">
+      <button
+        onClick={() => setExpanded((prev) => !prev)}
+        className={`${
+          {
+            GET: "bg-green-500/5",
+            POST: "bg-blue-500/5",
+            PATCH: "bg-yellow-500/5",
+            PUT: "bg-orange-500/5",
+            DELETE: "bg-red-500/5",
+          }[method]
+        } ${
+          {
+            GET: "text-green-100/70",
+            POST: "text-blue-100/70",
+            PATCH: "text-yellow-100/70",
+            PUT: "text-orange-100/70",
+            DELETE: "text-red-100/70",
+          }[method]
+        } rounded-md p-2 w-full`}
+      >
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-4">
+            <div
+              className={`text-lg tracking-widest rounded-sm font-semibold py-2 w-24 text-center ${
+                {
+                  GET: "text-green-500",
+                  POST: "text-blue-500",
+                  PATCH: "text-yellow-500",
+                  PUT: "text-orange-500",
+                  DELETE: "text-red-500",
+                }[method]
+              }`}
+            >
+              {method}
+            </div>
+            {docs?.access && (
+              <Icon
+                icon={
+                  {
+                    public: "tabler:lock-open",
+                    protected: "tabler:lock",
+                    private: "tabler:lock",
+                  }[docs.access]
+                }
+                className="size-5"
+              />
+            )}
+            <div className="text-lg tracking-wide">{path}</div>
+          </div>
+          <div className="flex items-center gap-4 mr-2">
+            {docs?.summary && (
+              <div className={` tracking-wide`}>{docs.summary}</div>
+            )}
+            {docs !== null && (
+              <span className={`p-2 rounded-md `}>
+                <Icon icon="tabler:chevron-down" className="size-5" />
+              </span>
+            )}
+          </div>
+        </div>
+      </button>
+      {docs !== null && (
+        <div
+          className={`${
+            expanded ? "mt-2 py-6 h-[calc-size(auto)]" : "mt-0 py-0 h-0"
+          } px-6 bg-zinc-900/50 overflow-hidden rounded-md space-y-8 transition-all`}
+        >
+          {docs.description && (
+            <section>
+              <h2 className="text-xl font-semibold tracking-wider text-zinc-10 mb-4">
+                Description
+              </h2>
+              <p className="text-zinc-500">{docs.description}</p>
+            </section>
+          )}
+          {(
+            [
+              ["params", "Parameters"],
+              ["query", "Query Parameters"],
+              ["body", "Request Body"],
+            ] as const
+          ).map(
+            ([key, title]) =>
+              docs[key].length > 0 && (
+                <section>
+                  <h2 className="text-xl tracking-wider font-semibold text-zinc-10 mb-4">
+                    {title}
+                  </h2>
+                  <table className="w-full">
+                    <thead>
+                      <tr className="border-b-2 border-zinc-800 text-zinc-500">
+                        <th className="px-2 py-2 text-left">Name</th>
+                        <th className="px-2 py-2">Type</th>
+                        <th className="px-2 py-2">Options</th>
+                        <th className="px-2 py-2">Required</th>
+                        <th className="px-2 py-2">Must Exist In DB</th>
+                        <th className="px-2 py-2 text-left">Description</th>
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y divide-zinc-800/50">
+                      {docs[key].map((item, i) => (
+                        <tr key={i}>
+                          <td className="px-2 py-2">{item.name}</td>
+                          <td className="px-2 py-2 text-center">
+                            <code>{item.type}</code>
+                          </td>
+                          <td className="px-2 py-2 text-center">
+                            {item.options?.length ? (
+                              item.options.join(", ")
+                            ) : (
+                              <span className="text-zinc-500">N/A</span>
+                            )}
+                          </td>
+                          <td className="px-2 py-2 text-center">
+                            {item.required ? (
+                              <span className="inline-flex flex-col items-center justify-center gap-2">
+                                <Icon
+                                  icon="tabler:check"
+                                  className="size-5 text-green-500"
+                                />
+                                {typeof item.required === "string" && (
+                                  <span className="text-center text-zinc-500">
+                                    ({item.required})
+                                  </span>
+                                )}
+                              </span>
+                            ) : (
+                              <Icon
+                                icon="tabler:minus"
+                                className="size-5 text-zinc-500 inline-block"
+                              />
+                            )}
+                          </td>
+                          <td className="px-2 py-2 text-center">
+                            {item.must_exist ? (
+                              <Icon
+                                icon="tabler:check"
+                                className="size-5 text-green-500 inline-block"
+                              />
+                            ) : (
+                              <Icon
+                                icon="tabler:minus"
+                                className="size-5 text-zinc-500 inline-block"
+                              />
+                            )}
+                          </td>
+                          <td className="px-2 py-2 text-zinc-500">
+                            {item.description}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </section>
+              )
+          )}
+          {docs.response && (
+            <section>
+              <h2 className="text-xl tracking-wider font-semibold text-zinc-10 mb-4">
+                Response
+              </h2>
+              <div className="flex items-center gap-4">
+                <div className="text-lg tracking-wide">
+                  <code>{docs.response.status}</code>
+                </div>
+                <div className="text-zinc-500 tracking-wide">
+                  {docs.response.description} -{" "}
+                  <code>{docs.response.body}</code>
+                </div>
+              </div>
+            </section>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PathEntry;
